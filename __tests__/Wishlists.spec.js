b2435c031fff17f96d088b76cc8bec7738b93a7b
nenber <berenger.piedagnel@free.fr>
2022-05-13 14:28:12 +0200

    test wishlist

--- __tests__/Wishlists.spec.js
+++ __tests__/Wishlists.spec.js
@@ -1,6 +1,9 @@
+
 const supertest = require("supertest");
 const db = require("./db");
 const client = supertest(require("../app.js"));
+const mongoose = require("mongoose");
+
 
 describe("test wishlist Api", () => {
   beforeAll(async () => await db.connect());
@@ -10,16 +13,33 @@ describe("test wishlist Api", () => {
     const response = await client.get("/api/wishlists");
     expect(response.status).toBe(200);
   });
+  it("should create a return code 200", async () => {
+    const response = await client
+      .post("/api/wishlists")
+      // .set("Content-Type", "application/json")
+      .send({
+        name: "Test wishlist",
+        items: []
+      });
+    expect(response.status).toBe(200);
+  });
   it("should create a new wishlist", async () => {
+    let objectId = mongoose.Types.ObjectId(1)
+    const item = { _id: objectId, name: "testitem", price: 4 }
     const response = await client
       .post("/api/wishlists")
       .set("Content-Type", "application/json")
       .send({
         name: "Test wishlist",
+        items: [item._id]
       });
-    expect(response.status).toBe(200);
+
     // expect(response.body).toHaveProperty("_id");
-    expect(response.req.data).toEqual({ name: "Test wishlist", items: [] });
+    expect(response.body.name).toBe("Test wishlist");
+    expect(response.body.items[0]).toEqual(objectId);
+
+    expect(response.status).toBe(200);
+
 
     //  expect(response.body.price).toBe(4);
   });
